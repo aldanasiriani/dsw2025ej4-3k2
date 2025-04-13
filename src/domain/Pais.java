562428d5d5ac98c5a98b6747713ccf27d78b31f6
aldanasiriani <aldanasiriani73@gmail.com>
2025-04-13 16:22:30 -0300

    Agrego clase Pais

---
+++ src/domain/Pais.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Aldana
+ */
+public class Pais {
+    private String nombre;
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+}
