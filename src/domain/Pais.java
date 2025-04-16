e1eec64d84db304738148e5708745d16a705e755
Camila Spinozzi <camilaspinozzif@gmail.com>
2025-04-15 21:58:51 -0300

    Agregado pais.java

---
+++ src/domain/Pais.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Spino
+ */
+public class Pais {
+   
+    private String nombre;
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
